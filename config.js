ace4b2a93fdf18387d8ce5da99da452d741cdef1
AngieMangie <99761717+AngieMangie@users.noreply.github.com>
2025-01-29 00:01:46 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,39 @@
+
+
+module.exports = {
+  TOKEN: "",
+  language: "en",
+  ownerID: ["743649979385774091", ""], 
+  mongodbUri : "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority ",
+  setupFilePath: './commands/setup.json',
+  commandsDir: './commands',  
+  embedColor: "#1db954",
+  activityName: "YouTube Music", 
+  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  embedTimeout: 5, 
+  errorLog: "", 
+  nodes: [
+    {
+      name: "INZEWORLD.COM (DE)",
+      password: "saher.inzeworld.com",
+      host: "lava.inzeworld.com",
+      port: 3128,
+      secure: false
+    },
+    {
+      name: "Catfein ID",
+      password: "catfein",
+      host: "lava.catfein.com",
+      port: 4000,
+      secure: false
+    },
+    { 
+      name: "saher",
+      password: "saher.inzeworld.com",
+      host: "lava.inzeworld.com",
+      port: 3128,
+      secure: false
+    }
+  ]
+}
